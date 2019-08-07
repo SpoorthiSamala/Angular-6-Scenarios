05cc476983eb0549e0fa93d5408fc7e1c9962c94
Spoorthi Samala <spoorthisamala@gmail.com>
2019-08-07 10:59:37 +0530

    Angular Scenario 5

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import{FormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {Component1Component} from './Scenario5/component1/component1.component';
+import {Component2Component} from './Scenario5/component2/component2.component';
+import {Component3Component} from './Scenario5/component3/component3.component';
+import { from } from 'rxjs';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Component1Component,
+    Component2Component,
+    Component3Component
+
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
